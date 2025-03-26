77f768fec42e3cc40565236beb9a99e0f21f39c1
satyPunk <satyamhere8@gmail.com>
2025-03-26 23:57:20 +0530

    Stage-10

--- index.js
+++ index.js
@@ -6,19 +6,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"))
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    res.render("meow.ejs");
 })
-// app.get("/write",(req,res)=>{
-//     res.render("write.ejs")
-// })
-// app.get("/read",(req,res)=>{
-//     res.render("read.ejs")
-// })
 app.get("/home",(req,res)=>{
     res.render("home.ejs");
 })
 app.get("/booking",(req,res)=>{
-    res.render("booking.ejs");
+    res.render("index.ejs");
 })
 app.get("/booking/agra",(req,res)=>{
     res.render("agra.ejs");
